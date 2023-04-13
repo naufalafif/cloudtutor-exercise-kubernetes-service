961a0ece051737e3947e49b1f9f92e99c3819466
naufal afif <naufalafif58@gmail.com>
2023-04-13 20:55:57 +0800

    update init script to wait for docker daemon

--- init_env.sh
+++ init_env.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sleep 3s
+echo "Preparing Machine"
+
+while ! docker info >/dev/null 2>&1; do
+    echo "Waiting for Docker daemon to start..."
+    sleep 1
+done
+
 kind create cluster --image=kindest/node:v1.26.3
+echo "Machine Ready"
